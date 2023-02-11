d28ae15f735fccdbe1f4f56d58c21d9ece8253e5
erasmorojastech <erasmorojastech@gmail.com>
2023-02-11 20:44:43 -0300

    Add a button that asks for a "Grid Size"
    This button on the top of the screen sends the user a popup asking for
    the number of squares per side for the new grid. Once entered, the
    existing grid is removed and a new grid is generated in the same total
    space as before. The maximum number of squares is set to 100.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,38 @@
 'use strict'
 
+const gridBtn = document.querySelector('#grid-size');
 const $board = document.querySelector('#board');
-let $grid = [];
+let gridSize;
 
-for (let i = 0; i < 256; i++) {
-  const cell = document.createElement('div');
-  cell.setAttribute('class', 'cell');
+gridBtn.addEventListener('click', () => {
+  gridSize = Number(prompt('Insert a Grid Size'));
 
-  $grid.push(cell);
-}
+  /* Validations */
+  if (Number.isInteger(gridSize) === false) return alert('Enter a valid Number between 2 and 100');
+  if (gridSize <= 1 || gridSize > 100) return alert('The Grid Size must be between 2 and 100');
 
-$board.replaceChildren(...$grid);
+  /* Rendering the Board */
+  let $grid = [];
 
-$board.childNodes.forEach((element)=>{
-  element.addEventListener('mouseenter', () => {
-    element.setAttribute('class', 'cell passed-over');
-  });
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let cellSide = 480 / gridSize;
+
+    const cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    cell.style.cssText = `width: ${cellSide}px; height: ${cellSide}px;`;
+
+    $grid.push(cell);
+  }
+
+  $board.replaceChildren(...$grid);
+});
+
+/* Adding Hover effect to the board cells */
+$board.addEventListener('mouseenter', ()=>{
+  const $display = $board.childNodes;
+  $display.forEach((element)=>{
+    element.addEventListener('mouseenter', () => {
+      element.setAttribute('class', 'cell passed-over');
+    });
+  })
 });
